e136d8e5daff10aa3b5f5cfc038087889e0fc65c
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-08-02 22:37:11 +0530

    final commit

--- apps/web/app/api/analytics/route.ts
+++ apps/web/app/api/analytics/route.ts
@@ -1,7 +1,24 @@
 import { prisma } from "@/lib/prisma";
+import client from "@/lib/redis";
 import { NextResponse } from "next/server";
 
 export async function GET(Request: Request) {
-    const analytics = await prisma.analyticsEvent.findMany({});
-    return NextResponse.json(analytics);
+    try {
+
+        let data = await client.get('analytics-data');
+        if(data) {
+            data = JSON.parse(data);
+            return NextResponse.json(data);
+        }
+        
+        const analytics = await prisma.analyticsEvent.findMany({});
+        const jsonString = JSON.stringify(analytics);
+
+        await client.set('analytics-data', jsonString);
+        await client.expire('analytics-data', 300);
+
+        return NextResponse.json(analytics);
+    }catch(err) {
+        return NextResponse.json({ message: 'Something went wrong' }, { status: 500 });
+    }
 }
